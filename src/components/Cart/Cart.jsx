2cd4c6fc025988e605845403f51e7368c2e0d754
Mollakhaled <khaledbalok@gmail.com>
2023-03-28 14:52:46 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,12 +4,18 @@ import './Cart.css';
 const Cart = ({cart}) => {
   // const cart = props.cart; //option 1
   // const {cart} = props;  //option 2
-  console.log(cart);
+  // console.log(cart);
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for(const product of cart){
-    totalPrice = totalPrice + product.price;
+  //  if(product.quantity === 0){
+  //     product.quantity = 1;
+  //  }
+  //   product.quantity = product.quantity || 1; 
+    totalPrice = totalPrice + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
   const tax = totalPrice*7/100;
   const grandTotal = totalPrice + totalShipping + tax ;
@@ -19,7 +25,7 @@ const Cart = ({cart}) => {
   return (
     <div className='cart'>
       <h4>order summary</h4>
-      <p>Selected Items:{cart.length}</p>
+      <p>Selected Items:{quantity}</p>
       <p>Total Price:${totalPrice}</p>
       <p>Total Shipping Charge:${totalShipping}</p>
       <p>Tax:{tax.toFixed(2)}</p>
