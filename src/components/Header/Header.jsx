455adb9c03534cf063489c9ed2a76bedbd8a9e52
Mollakhaled <khaledbalok@gmail.com>
2023-05-02 11:38:10 +0600

    sign up and context api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import('./Header.css');
 import logo from '../../images/Logo.svg';
 import{Link} from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 const Header = () => {
+ 
+  const {user}= useContext(AuthContext);
+
   return (
     <nav className='header'>
       <img src={logo} alt="" />
@@ -10,7 +14,9 @@ const Header = () => {
         <Link to="/">Shop</Link>
         <Link to="/orders">Orders</Link>
         <Link to="/inventory">Inventory</Link>
-        <Link to="login">Login</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign up</Link>
+        {user && <span>Welcome</span>}
       </div>
 
     </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -15,32 +15,38 @@ import Inventory from './components/Inventory/Inventory';
 import Login from './components/Login/Login';
 import cartProductsLoader from './loaders/cartProductsLoader';
 import Checkout from './components/Checkout/Checkout';
+import SignUp from './components/SignUp/SignUp';
+import AuthProvider from './components/providers/AuthProvider';
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<Home></Home>,
+    path: '/',
+    element: <Home></Home>,
     children: [
       {
-        path:'/',
-        element:<Shop></Shop>,
+        path: '/',
+        element: <Shop></Shop>,
       },
       {
-        path:'orders',
-        element:<Orders></Orders>,
-        loader:cartProductsLoader
+        path: 'orders',
+        element: <Orders></Orders>,
+        loader: cartProductsLoader
       },
       {
-        path:'inventory',
-        element:<Inventory></Inventory>,
+        path: 'inventory',
+        element: <Inventory></Inventory>,
       },
       {
-        path:'login',
-        element:<Login></Login>,
+        path: 'login',
+        element: <Login></Login>,
       },
       {
-        path:'checkout',
-        element:<Checkout></Checkout>,
+        path: 'checkout',
+        element: <Checkout></Checkout>,
+      },
+      {
+        path: '/signup',
+        element: <SignUp></SignUp>
       }
     ]
   }
@@ -48,6 +54,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
